80a77c8d0fe8ec3d69dcbf3a8c4008fe5570e673
Alex Smirnov <sas2job@gmail.com>
2021-06-22 11:58:43 +0300

    Add author's email to json article

--- app/serializers/article_serializer.rb
+++ app/serializers/article_serializer.rb
@@ -4,4 +4,7 @@ class ArticleSerializer
   include JSONAPI::Serializer
   attributes :id, :title, :body, :category, :published_date, :user_id
   set_key_transform :camel_lower
+  attribute :name do |object|
+    object.user.email
+  end
 end
